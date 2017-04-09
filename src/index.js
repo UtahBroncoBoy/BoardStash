1c38f6888a9f30a6327ed771fedcc475d23b8500
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-04-09 09:47:34 -0600

    Changes to Index

--- src/index.js
+++ src/index.js
@@ -10,15 +10,20 @@ import {loadRateLevelData} from './data/data';
 import BillboardRoute from './routes/billboardRoute';
 import RateLevelRoute from './routes/rateLevelRoute';
 
+//determine type of environment
+const ENV = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 //create express application
 const app = Express();
 
 //assign port 
 const port = process.env.PORT || 5000
 
-//load data sets
+//load data sets if it is a development environment
+if(ENV == 'development'){
 loadBillboardData();
 loadRateLevelData();
+}
 
 //use body-parser in express application
 app.use(BodyParser.json());
