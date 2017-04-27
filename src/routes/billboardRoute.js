ba75247b903fc8dbcb7535416bb4dedb04ad258a
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-04-27 15:13:55 -0600

    Added route to get one billboard by _id.

--- src/routes/billboardRoute.js
+++ src/routes/billboardRoute.js
@@ -3,6 +3,8 @@ import {Router} from 'Express';
 
 //import getCollection function from db.js (Which connects to the database and retrieves a collection.)
 import {getCollection} from '../db';
+import {billboards} from '../data/data';
+import Billboard from '../data/Billboard';
 
 //declare router
 const router = new Router();
@@ -14,6 +16,13 @@ const getAllBillboards = async() => {
   return await ( await billboards.find({})).toArray();
 }
 
+//function used to retrieve and return one billboard from the collection
+const getBillboard = async(_id) => {
+  const billboardCollection = await getCollection('billboards');
+  const billboard = await (await billboardCollection.find({_id})).toArray();
+  return billboard;
+};
+
 //set route to retrieve all billboards using the getAllBillboards function
 router.get('/', (req, res) => {
   return getAllBillboards()
@@ -21,6 +30,17 @@ router.get('/', (req, res) => {
       console.log(billboards);
       return res.json(billboards);
     });
+});
+
+
+//set route to retrieve one billboard
+router.get('/:billboard', (req, res) => {
+  return getBillboard(req.params.billboard).then(
+    billboard => {
+      console.log(billboard);
+      return res.json(billboard);
+    }
+  )
 });
 
 //export router to find all billboards
